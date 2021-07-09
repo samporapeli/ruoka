4221d86aa4ce20ef52114c784c33207fdce1c682
Felix Bade <flix.bade@gmail.com>
2021-07-09 10:04:24 +0300

    Add max_upload_size

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from secret import upload_folder
+from secret import upload_folder, max_upload_size
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = upload_folder
+app.config['MAX_CONTENT_LENGTH'] = max_upload_size
 
 from app import views
