84984ffb876ee4187ac5e1d09e81b16cb0b23b54
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-07-09 09:55:38 +0300

    Add image upload view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/')
 def view_frontpage():
     return render_template('frontpage.html')
+
+@app.route('/create', methods=['GET'])
+def view_create():
+    return render_template('create.html')
+
+@app.route('/create', methods=['POST'])
+def handle_create():
+    # TODO: actually save the images
+    return redirect(url_for('view_frontpage'))
