aab4019cf4d42a6c8d6c673e46952ba785b27056
Felix Bade <flix.bade@gmail.com>
2021-07-09 08:54:41 +0300

    Add flask entrypoint (main)

---
+++ ruoka.py
@@ -0,0 +1,15 @@
+import logging
+
+import werkzeug.serving
+
+from app import app
+from secret import port, debug
+
+# we are behind a proxy. log the ip of the end-user, not the proxy.
+# this will also work without the proxy
+werkzeug.serving.WSGIRequestHandler.address_string = lambda self: self.headers.get('x-real-ip', self.client_address[0])
+
+# log to a file (access.log), not stderr
+logging.basicConfig(filename='access.log', level=logging.DEBUG, format='%(message)s')
+
+app.run(port=port, debug=debug, use_reloader=debug)
