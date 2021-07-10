da524c80a6c5825a08fac1bddcde9306da18e892
Felix Bade <flix.bade@gmail.com>
2021-07-10 17:02:26 +0300

    Show images in newest first order

--- app/views/frontpage.py
+++ app/views/frontpage.py
@@ -5,5 +5,5 @@ from app.models.image import Image
 
 @app.route('/')
 def view_frontpage():
-    images = Image.query.all()
+    images = Image.query.order_by(Image.id.desc()).all()
     return render_template('frontpage.html', images=images)
