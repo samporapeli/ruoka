da17ff417e20668c692a2c04434dafdab745887f
Felix Bade <flix.bade@gmail.com>
2021-07-10 15:49:42 +0300

    Add image links to image list

--- app/models/image.py
+++ app/models/image.py
@@ -1,8 +1,13 @@
+from flask import url_for
 from app import db
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100), unique=True, nullable=False)
+
+    @property
+    def url(self):
+        return url_for('serve_image', path=self.filename)
 
     def __repr__(self):
         return '<Image %r>' % self.id
