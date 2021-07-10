9cea94f23e228518c03e50ed213fae9a71f07633
Felix Bade <flix.bade@gmail.com>
2021-07-10 23:15:52 +0300

    Add user relation to Image

--- app/models/image.py
+++ app/models/image.py
@@ -5,6 +5,11 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(100), unique=True, nullable=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    user = db.relationship('User',
+        backref=db.backref('images', lazy=True))
+
     @property
     def url(self):
         return url_for('serve_image_by_uuid', path=self.filename)
