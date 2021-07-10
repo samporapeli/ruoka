a516f3c2d983bd51cdbb4f5aeadb3728db035c1e
Felix Bade <flix.bade@gmail.com>
2021-07-10 19:29:17 +0300

    Resize images to max 1536px before saving

--- app/views/image_uploads.py
+++ app/views/image_uploads.py
@@ -2,13 +2,29 @@ from random import random
 from os import path
 from uuid import uuid4
 
-from app import app, db
 from flask import render_template, request, redirect, url_for, send_from_directory, abort
+import PIL.Image
 
+from app import app, db
 from app.models.image import Image
 
 def random_filename():
     return str(uuid4())
+
+# 1536 = 1.5 * 1024
+def save_as_thumbnail(file, io, max_side_length=1536):
+    image = PIL.Image.open(file)
+    width, height = image.size
+
+    multiplier = max_side_length / max(width, height)
+    multiplier = min(1, multiplier) # don't enlarge image
+    new_size = (width * multiplier, height * multiplier)
+
+    image.thumbnail(new_size)
+    image_format = file.content_type.split('/')[1]
+    image.save(io, image_format)
+
+
 
 def save_uploaded_image(file):
     if file.filename == '': 
@@ -25,7 +41,9 @@ def save_uploaded_image(file):
     filename = random_filename() + extension
     db.session.add(Image(filename=filename))
     db.session.commit()
-    file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
+    save_path = path.join(app.config['UPLOAD_FOLDER'], filename)
+    with open(save_path, 'w') as f:
+        save_as_thumbnail(file, f)
 
     return True
 
