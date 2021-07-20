6f3368661fd376fb9870f674db89b719e80592d4
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-07-20 23:37:39 +0300

    Refactor upload handling to its own view

--- app/views/frontpage.py
+++ app/views/frontpage.py
@@ -1,19 +1,11 @@
 from flask import render_template, request, abort
 
 from app import app, db
-from app.views.image_uploads import save_uploaded_image
 from app.views.user import get_user
 from app.models.image import Image
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def view_frontpage():
     user = get_user()
-    if request.method == 'POST':
-        if 'image' in request.files:
-            file = request.files['image']
-            if user is None:
-                abort(403)
-            save_uploaded_image(file, user=user)
-
     images = Image.query.order_by(Image.id.desc()).all()
     return render_template('frontpage.html', images=images, user=user)
