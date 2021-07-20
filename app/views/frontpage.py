3187a052d40ca53f0541d3024e6958791277f7af
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-07-20 23:56:11 +0300

    Refactor frontpage to use get_user() directly
    This commit introduces get_user() method globally for all templates and
    refactors frontpage to use that instead of passing the user information
    from view as a variable.

--- app/views/frontpage.py
+++ app/views/frontpage.py
@@ -1,11 +1,9 @@
 from flask import render_template, request, abort
 
 from app import app, db
-from app.views.user import get_user
 from app.models.image import Image
 
 @app.route('/', methods=['GET'])
 def view_frontpage():
-    user = get_user()
     images = Image.query.order_by(Image.id.desc()).all()
-    return render_template('frontpage.html', images=images, user=user)
+    return render_template('frontpage.html', images=images)
