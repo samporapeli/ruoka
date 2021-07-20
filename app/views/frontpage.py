2bf462181b24274a22ecec5dde745df91cc3811c
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-07-21 00:09:03 +0300

    Remove unnecessary argument
    From Flask's documentation
    (https://flask.palletsprojects.com/en/2.0.x/quickstart/#http-methods):
    "By default, a route only answers to GET requests.
    You can use the methods argument of the route()
    decorator to handle different HTTP methods."
    Thus, methods=['GET'] can be omitted, as @felixbade mentions in comment
    https://github.com/samporapeli/ruoka/pull/5#commitcomment-53751668

--- app/views/frontpage.py
+++ app/views/frontpage.py
@@ -3,7 +3,7 @@ from flask import render_template, request, abort
 from app import app, db
 from app.models.image import Image
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def view_frontpage():
     images = Image.query.order_by(Image.id.desc()).all()
     return render_template('frontpage.html', images=images)
