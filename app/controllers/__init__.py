8d6da314378676a5efc946502a2470eef03c7ef5
Felix Bade <flix.bade@gmail.com>
2021-07-21 19:44:24 +0300

    mv app/views -> app/controllers

---
+++ app/controllers/__init__.py
@@ -0,0 +1,6 @@
+import app.controllers.frontpage
+import app.controllers.upload_handler
+import app.controllers.image_uploads
+import app.controllers.image_info
+import app.controllers.user
+import app.controllers.profile
--- app/views/__init__.py
+++
@@ -1,6 +0,0 @@
-import app.views.frontpage
-import app.views.upload_handler
-import app.views.image_uploads
-import app.views.image_info
-import app.views.user
-import app.views.profile
