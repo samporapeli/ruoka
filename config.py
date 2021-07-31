bd81f1a7876ccfa1459aed72d2bc918cd2feb3f8
Felix Bade <flix.bade@gmail.com>
2021-07-31 22:24:37 +0300

    Fix bug with upload size being a str

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     SECRET_KEY = env_or_exit('SECRET_KEY')
 
     UPLOAD_FOLDER = env_or_exit('UPLOAD_FOLDER')
-    MAX_CONTENT_LENGTH = env_or_exit('MAX_UPLOAD_SIZE')
+    MAX_CONTENT_LENGTH = int(env_or_exit('MAX_UPLOAD_SIZE'))
 
     SQLALCHEMY_DATABASE_URI = env_or_exit('DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
