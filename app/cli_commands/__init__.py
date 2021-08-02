bf059378485adcff52ddde4701998c656520a867
Felix Bade <flix.bade@gmail.com>
2021-08-02 19:23:13 +0300

    Add populate-dev-db cli command and refactor add_user command

--- app/cli_commands/__init__.py
+++ app/cli_commands/__init__.py
@@ -5,8 +5,17 @@ from app.models.user import User
 
 # https://flask.palletsprojects.com/en/2.0.x/cli/#custom-commands
 
-@app.cli.command("add-user")
-@click.argument("name")
-def add_dummy_users(name):
+def add_user(name):
     db.session.add(User(username=name))
     db.session.commit()
+
+@app.cli.command("add-user")
+@click.argument("name")
+def cli_add_dummy(name):
+    add_user(name)
+
+@app.cli.command("populate-dev-db")
+def cli_populate_dev_db():
+    add_user('devuser1')
+    add_user('devuser2')
+    add_user('devuser3')
